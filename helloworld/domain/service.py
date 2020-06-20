2b09f10b1ecb3408c49e6453c44819d144cda847
yuya okada <yuya.okada527@gmail.com>
2020-06-21 00:30:02 +0900

    remove lru cache

--- helloworld/domain/service.py
+++ helloworld/domain/service.py
@@ -1,4 +1,3 @@
-from functools import lru_cache
 from typing import List
 
 from helloworld.domain.models import WorkBook
@@ -8,14 +7,12 @@ from helloworld.infra.s3 import S3Client
 WORKBOOK_PREFIX = "materials/workbooks"
 
 
-@lru_cache
 def get_workbooks() -> List[WorkBook]:
     workbook_objects = S3Client.list_objects(WORKBOOK_PREFIX)
     workbook_keys = [obj.key for obj in workbook_objects]
     return [WorkBook(key, key.split("/")[-1]) for key in workbook_keys if not key.endswith("/")]
 
 
-@lru_cache
 def get_workbook(key: str):
     workbook_object = S3Client.get_object(key)
     return workbook_object.get()["Body"].read()
